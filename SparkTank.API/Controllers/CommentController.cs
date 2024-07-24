5fcfb894430d2174fbdff616cfccfe47f60655ae
Abdulsemed <xbebe346@gmail.com>
2024-07-24 21:55:26 +0300

    feat: comment controller

--- SparkTank.API/Controllers/CommentController.cs
+++ SparkTank.API/Controllers/CommentController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SparkTank.Application.Responses;
+using System.Security.Claims;
+using tech.Application.DTOs.CommentsDto;
+using tech.Application.Features.Comment.Request.Commands;
 
 namespace tech.API.Controllers
 {
@@ -7,5 +13,37 @@ namespace tech.API.Controllers
     [ApiController]
     public class CommentController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public CommentController(IMediator mediator, IHttpContextAccessor httpContextAccessor)
+        {
+            _mediator = mediator;
+            _contextAccessor = httpContextAccessor;
+        }
+
+        [HttpPost]
+        [Route("CreateComment")]
+        [Authorize]
+        public async Task<ActionResult<BaseResponseClass>> CreateComment([FromBody] CreateCommentDto createCommentDto)
+        {
+            var userId = new Guid(_contextAccessor.HttpContext!.User.FindFirstValue("userid"));
+            var command = new CreateCommentCommand { CreateCommentDto = createCommentDto, UserId = userId };
+            var result = await _mediator.Send(command);
+            return StatusCode(result.StatusCode, result);
+
+        }
+
+        [Authorize]
+        [HttpPut]
+        [Route("UpdateComment")]
+        public async Task<ActionResult<BaseResponseClass>> UpdateComment([FromBody] UpdateCommentDto updateCommentDto)
+        {
+            var userId = new Guid(_contextAccessor.HttpContext!.User.FindFirstValue("userid"));
+            var command = new UpdateCommentCommand { UpdateCommentDto = updateCommentDto, UserId = userId } ;
+            var result = await _mediator.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
+
